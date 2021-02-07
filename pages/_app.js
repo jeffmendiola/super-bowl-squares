3d852e74d4eeb56edce491a245bced19da2e851b
Marc Mendiola <marc@goodrx.com>
2021-02-07 05:55:55 -0800

    Fix material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
+import { useEffect } from 'react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}/>
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp
