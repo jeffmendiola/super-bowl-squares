17918a49cf5960e4a5bbbb4a7385385f6189d971
Marc Mendiola <marc@goodrx.com>
2021-02-07 05:36:47 -0800

    Add env

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import '../styles/globals.css'
 
-import { useEffect, useState } from 'react';
-
 function MyApp({ Component, pageProps }) {
-  const [squareConfig, setSquareConfig] = useState({})
-  useEffect(() => {
-    fetch('/api/square')
-      .then((res) => res.json())
-      .then((squareConfig) => setSquareConfig(squareConfig))
-  });
-
-  return <Component {...pageProps} squareConfig={squareConfig}/>
+  return <Component {...pageProps}/>
 }
 
 export default MyApp
