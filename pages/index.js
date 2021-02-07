995f931694bbcba5ef08caa61f198d6013b7281f
jeffmendiola <jeffwmendiola@gmail.com>
2021-02-07 05:12:54 -0800

    Create football squares grid

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,380 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+// import styles from "../styles/Home.module.css";
+import { Box, Container, Divider, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+
+const useStyles = makeStyles({
+  root: {},
+  columnHeaderSquares: {
+    height: 20,
+    width: 80,
+    textAlign: "center",
+  },
+  columnTeam: {
+    position: "absolute",
+    top: 0,
+    left: 32,
+    textAlign: "center",
+    width: "100%",
+  },
+  columns: {
+    position: "absolute",
+    top: 24,
+    left: 56,
+  },
+  container: {
+    position: "relative",
+    marginTop: 24,
+    width: 896,
+  },
+  grid: {
+    position: "absolute",
+    top: 56,
+    left: 56,
+  },
+  rowHeaderSquares: {
+    height: 80,
+    lineHeight: 5,
+    width: 20,
+    textAlign: "center",
+  },
+  rowTeam: {
+    position: "absolute",
+    transform: "rotate(-90deg)",
+    width: "100%",
+    textAlign: "center",
+    top: 435,
+    right: 433,
+  },
+  rows: {
+    position: "absolute",
+    top: 56,
+    left: 24,
+  },
+  square: {
+    height: 80,
+    width: 80,
+  },
+  title: {
+    fontSize: 56,
+  },
+  hightlight: {
+    backgroundColor: "#fff200",
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Super Bowl Squares</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Container maxWidth="md" style={{ marginTop: 32 }}>
+          <Typography className={classes.title} variant="h1">
+            Super Bowl LV
+          </Typography>
+          <Divider />
+          <div className={classes.container}>
+            <Typography className={classes.columnTeam} variant="subtitle1">
+              Tampa Bay Buccaneers
+            </Typography>
+            <Typography className={classes.rowTeam} variant="subtitle1">
+              Kansas City Chiefs
+            </Typography>
+            <Box
+              className={classes.columns}
+              display="flex"
+              flexDirection="row"
+              flexWrap="wrap"
+              p={1}
+              m={1}
+              bgcolor="background.paper"
+            >
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                5
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                7
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                4
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                2
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                0
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                1
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                6
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                8
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                9
+              </Paper>
+              <Paper
+                className={classes.columnHeaderSquares}
+                variant="outlined"
+                square
+              >
+                3
+              </Paper>
+            </Box>
+            <Box
+              className={classes.rows}
+              display="flex"
+              flexDirection="column"
+              flexWrap="wrap"
+              p={1}
+              m={1}
+              bgcolor="background.paper"
+            >
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                0
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                6
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                3
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                2
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                4
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                1
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                7
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                8
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                5
+              </Paper>
+              <Paper
+                className={classes.rowHeaderSquares}
+                variant="outlined"
+                square
+              >
+                9
+              </Paper>
+            </Box>
+            <Box
+              className={classes.grid}
+              display="flex"
+              flexDirection="row"
+              flexWrap="wrap"
+              p={1}
+              m={1}
+              bgcolor="background.paper"
+            >
+              {/* ========================================================== */}
+              <Paper
+                className={clsx(classes.square, classes.hightlight)}
+                variant="outlined"
+                square
+              />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              <Paper className={classes.square} variant="outlined" square />
+              {/* ========================================================== */}
+            </Box>
+          </div>
+        </Container>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <footer />
+    </>
+  );
 }
